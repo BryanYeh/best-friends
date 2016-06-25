5dad59f9e6239a75a13bfbcf3eb1027586f1cbba
Bryan Y <cs337bryan@gmail.com>
2016-06-25 11:43:44 -0700

    video 10

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,33 +7,28 @@ use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-  public function getDashboard()
-  {
-    return view('dashboard');
-  }
+   public function postSignUp(Request $request)
+   {
+      $this->validate($request, [
+         'email' => 'required|email|unique:users',
+         'first_name' => 'required|max:120',
+         'password' => 'required|min:4'
+      ]);
+      $email = $request['email'];
+      $first_name = $request['first_name'];
+      $password = bcrypt($request['password']);
 
-  public function postSignUp(Request $request)
-  {
-    $this->validate($request, [
-      'email' => 'required|email|unique:users',
-      'first_name' => 'required|max:120',
-      'password' => 'required|min:4'
-    ]);
-    $email = $request['email'];
-    $first_name = $request['first_name'];
-    $password = bcrypt($request['password']);
+      $user = new User();
+      $user->email = $email;
+      $user->first_name = $first_name;
+      $user->password = $password;
 
-    $user = new User();
-    $user->email = $email;
-    $user->first_name = $first_name;
-    $user->password = $password;
+      $user->save();
 
-    $user->save();
+      Auth::login($user);
 
-    Auth::login($user);
-
-    return redirect()->route('dashboard');
-  }
+      return redirect()->route('dashboard');
+   }
 
   public function postSignIn(Request $request)
   {
